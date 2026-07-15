f7aaff577140c8ba83a1987fe0b42404ea3fedb7
Nishant-gupta16 <nishant7524044987@gmail.com>
2026-07-15 17:36:36 +0530

    Done

---
+++ code.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+
+// int main(){
+//     cout << "Nishant";
+// }
+
+int main(){
+    // int n=3;
+    // int sum=0;
+    // int i=1;
+    // while(i<=n){
+    //     sum=sum+i;
+    //     i++;
+        
+    // }
+    // cout << sum;
+    // return 0;
+
+
+    // int n=5;
+    // int fact=1;
+    // for(int i=1; i<=n;i++){
+    //     fact=fact*i;
+    // }
+    // cout << fact;
+    // return 0;
+
+
+    // int n=7;
+    // for(int i=2; i<=n-1;i++){
+    //     if (n%i==0){
+    //         cout<<"not prime";
+    //     }
+    //     else{
+    //         cout << "prime";
+    //     }
+    //     break;
+    // }
+    // return 0 ;
+
+
+    // string name;
+    // cout<<"enter Your name:- ";
+    // cin>>name;
+    // cout<<name;
+    // return 0;
+
+    // int a;
+    // int b;
+
+    // cout<<"Enter value :- ";
+    // cin>>a;
+    // cout<<"Enter value :- ";
+    // cin>>b;
+
+    // int sum=a+b;
+    // cout<<"sum of numbers is :- "<<sum;
+
+    
+}
---
+++ code.js
@@ -0,0 +1,87 @@
+// let n=6;
+// let sum=0;
+// let count=1;
+
+// // if(sum>=n){
+// //     sum=sum+count;
+// //     console.log(sum)
+// // }
+// // for(let i=0; i<=n;i++){
+// //     sum=sum+i;
+    
+// // }
+
+// // console.log(sum)
+
+// // if (n>=sum){
+// //     sum=sum+count;
+// //     count++
+// //     console.log(sum)
+// // }
+
+// while(count<=n){
+//     sum=sum+count;
+//     count++;
+// }
+// console.log(sum)
+
+
+// let n=99;
+// let i=2;
+// while(i<=n){
+//     if (n%i==0){
+//         console.log(`not prime`)
+//     }
+//     else{
+//         console.log(`prime`)
+//     }
+//     break
+// }
+
+
+// let n=5;
+// let value=1;
+// let i=1;
+
+// while(i<=n){
+//     value=value*i;
+//     i++
+// }
+// console.log(value);
+
+// let n=5;
+// let value=1;
+// for(let i=1;i<=n;i++){
+//     value*=i
+// }
+// console.log(value)
+
+// let a =28;
+// let b=23;
+
+// if(a>b){
+//     console.log(a+" max")
+// }
+// else{
+//     console.log(b+"max")
+// }
+
+
+// let age=12;
+
+// if(age>=18){
+//     console.log(" get license")
+// }
+// else{
+//     console.log("not get license")
+// }
+
+
+// let p=100;
+// let r=23;
+// let t= 2;
+
+// let si=(p*r*t)/100
+
+// console.log(si)
+

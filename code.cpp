252236150eff02af6c67b1dfef583a7661b76721
Nishant-gupta16 <nishant7524044987@gmail.com>
2026-07-16 19:01:07 +0530

    done

--- code.cpp
+++ code.cpp
@@ -1,61 +1,169 @@
 #include<iostream>
 using namespace std;
 
-// int main(){
-//     cout << "Nishant";
-// }
+int main()
+{
+    // Question 1: Print "Nishant".
+    // cout<<"Nishant";
 
-int main(){
+    // ======================================================
+
+    // Question 2: Find the sum of numbers from 1 to n using while loop.
     // int n=3;
     // int sum=0;
     // int i=1;
+
     // while(i<=n){
-    //     sum=sum+i;
+    //     sum+=i;
     //     i++;
-        
     // }
-    // cout << sum;
-    // return 0;
+    // cout<<sum;
 
+    // ======================================================
 
-    // int n=5;
+    // Question 3: Find the factorial of a number.
+    // int num=5;
     // int fact=1;
-    // for(int i=1; i<=n;i++){
-    //     fact=fact*i;
+
+    // for(int i=1;i<=num;i++){
+    //     fact*=i;
     // }
-    // cout << fact;
-    // return 0;
+    // cout<<fact;
 
+    // ======================================================
 
-    // int n=7;
-    // for(int i=2; i<=n-1;i++){
-    //     if (n%i==0){
-    //         cout<<"not prime";
-    //     }
-    //     else{
-    //         cout << "prime";
-    //     }
-    //     break;
+    // Question 4: Check whether a number is Prime or Not.
+
+    // int num=7;
+    // bool isPrime=true;
+
+    // if(num<=1){
+    //     isPrime=false;
     // }
-    // return 0 ;
 
+    // for(int i=2;i*i<=num;i++){
+    //     if(num%i==0){
+    //         isPrime=false;
+    //         break;
+    //     }
+    // }
 
+    // if(isPrime){
+    //     cout<<"Num is Prime";
+    // }
+    // else{
+    //     cout<<"Num is Not Prime";
+    // }
+
+    // ======================================================
+
+    // Question 5: Take name as input and print it.
     // string name;
-    // cout<<"enter Your name:- ";
+    // cout<<"Enter Name : ";
     // cin>>name;
     // cout<<name;
-    // return 0;
 
-    // int a;
-    // int b;
+    // ======================================================
 
-    // cout<<"Enter value :- ";
+    // Question 6: Find the sum of two numbers.
+    // int a,b;
+    // cout<<"Enter First Number : ";
     // cin>>a;
-    // cout<<"Enter value :- ";
+
+    // cout<<"Enter Second Number : ";
     // cin>>b;
 
-    // int sum=a+b;
-    // cout<<"sum of numbers is :- "<<sum;
+    // cout<<"Sum = "<<a+b;
 
-    
+    // ======================================================
+
+    // Question 7: Check whether a character is lowercase or uppercase.
+    // char ch;
+    // cout<<"Enter Character : ";
+    // cin>>ch;
+
+    // if(ch>='a' && ch<='z'){
+    //     cout<<"Lowercase";
+    // }
+    // else{
+    //     cout<<"Uppercase";
+    // }
+
+    // ======================================================
+
+    // Question 8: Print ASCII value of a character.
+    // char ch;
+    // cout<<"Enter Character : ";
+    // cin>>ch;
+
+    // cout<<(int)ch;
+
+    // ======================================================
+
+    // Question 9: Print all even numbers from 1 to n and find their sum.
+    // int num;
+    // int sum=0;
+
+    // cout<<"Enter Number : ";
+    // cin>>num;
+
+    // for(int i=1;i<=num;i++){
+
+    //     if(i%2==0){
+    //         cout<<i<<endl;
+    //         sum+=i;
+    //     }
+
+    // }
+
+    // cout<<"Sum = "<<sum;
+
+    // ======================================================
+
+    // Question 10: Print all odd numbers from 1 to n and find their sum.
+    // int num=30;
+    // int sum=0;
+
+    // for(int i=1;i<=num;i++){
+
+    //     if(i%2!=0){
+
+    //         cout<<i<<endl;
+    //         sum+=i;
+
+    //     }
+
+    // }
+
+    // cout<<"Sum = "<<sum;
+
+    // ======================================================
+
+    // Question 11: Print numbers from 1 to n using do-while loop.
+    // int num=20;
+    // int i=1;
+
+    // do{
+    //     cout<<i<<endl;
+    //     i++;
+    // }
+    // while(i<=num);
+
+    // ======================================================
+
+    // Question 12: Find the sum of numbers divisible by 3.
+    // int num=23;
+    // int sum=0;
+
+    // for(int i=1;i<=num;i++){
+
+    //     if(i%3==0){
+    //         sum+=i;
+    //     }
+
+    // }
+
+    // cout<<sum;
+
+    return 0;
 }

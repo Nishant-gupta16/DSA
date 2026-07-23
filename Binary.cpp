be5c1f918a0bf05c7f7ac4a4607bf780277e9609
Nishant-gupta16 <nishant7524044987@gmail.com>
2026-07-23 19:45:38 +0530

    done

--- Binary.cpp
+++ Binary.cpp
@@ -1,46 +1,75 @@
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+
+
+// // Q1. Decimal to Binary------------.
+// int toBinary(int num){
+//     int pow=1;
+//     int ans=0;
+
+//     while(num>0){
+//         int rem=num%2;
+//         num=num/2;
+
+//         ans=ans+(pow*rem);
+//         pow*=10;
+
+//     }
+
+//     cout<<"Binary value is:- "<<ans<<endl;
+// }
+
+
+// // Q2. Binary to Decimal-------------------.
+// int toDecimal(int binaryNum)
+// {
+//     int ans = 0;
+//     int pow = 1;
+
+//     while (binaryNum > 0)
+//     {
+//         int rem = binaryNum % 10;
+//         binaryNum = binaryNum / 10;
+
+//         ans = ans + (rem * pow);
+
+//         pow = pow * 2;
+//     }
+
+//     cout <<"Decimal value is:- "<< ans;
+// }
+
+// int main()
+// {
+//     toBinary(6);
+//     toDecimal(101);
+// }
+
+
+
+#include<iostream>
 using namespace std;
 
+int main(){
+     
 
-// Q1. Decimal to Binary------------.
-int toBinary(int num){
-    int pow=1;
-    int ans=0;
+    // Question:
+    // Write a C++ program to check whether a given number is a Power of 2
+    // using the Bitwise AND (&) operator.
+    // If the number is a Power of 2, print "Power of 2",
+    // otherwise print "Not power of 2".
 
-    while(num>0){
-        int rem=num%2;
-        num=num/2;
+    // int num = 16;
 
-        ans=ans+(pow*rem);
-        pow*=10;
+    // if (num > 0 && (num & (num - 1)) == 0) {
+    //     cout << "Power of 2";
+    // }
+    // else {
+    //     cout << "Not power of 2";
+    // }
 
-    }
+    // return 0;
 
-    cout<<"Binary value is:- "<<ans<<endl;
-}
+  
 
-
-// Q2. Binary to Decimal-------------------.
-int toDecimal(int binaryNum)
-{
-    int ans = 0;
-    int pow = 1;
-
-    while (binaryNum > 0)
-    {
-        int rem = binaryNum % 10;
-        binaryNum = binaryNum / 10;
-
-        ans = ans + (rem * pow);
-
-        pow = pow * 2;
-    }
-
-    cout <<"Decimal value is:- "<< ans;
-}
-
-int main()
-{
-    toBinary(10);
-    toDecimal(101);
 }

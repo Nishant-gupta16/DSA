0adc1e02fee9248d1e2c1c739a3b2432fc4647ae
Nishant-gupta16 <nishant7524044987@gmail.com>
2026-09-25 09:39:20 +0530

    done

---
+++ exercise.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+using namespace std;
+
+// int main (){
+//       int a=4;
+//       int b=5;
+//       int sum=a+b;
+//          cout<<sum;
+
+// }
+
+// int main (){
+//     string a;
+//     cout<<"Enter your name:-";
+//     cin>>a;
+//     cout<<a;
+// }
+
+
+// int main(){
+//     string name;
+//     int age;
+//     cout<<"Enter your name:-";
+//     cin>>name;
+//     cout<<"Age:-";
+//     cin>>age;
+//      if(age>=18 && age <=60){
+//         cout<<"You are eligible to Drive.";
+//      }
+//      else if(age >= 60){
+//         cout<<"You are not eligible because your age is over.";
+//      }
+//      else{
+//         cout<<"You are not eligible to Drive.";
+//      }
+//      return 0;
+// }
+
+// int main(){
+//     char ch;
+//      cout<<"Enter Character:-";
+//      cin>>ch;
+
+//      if(ch>='a' && ch<='z'){
+//         cout<<"To Lowercase";
+//      }
+//      else{
+//         cout<<"TO Uppercase";
+//      }
+// }
+
+// int main(){
+//     char ch;
+//      cout<<"Enter Character:-";
+//      cin>>ch;
+//      ch>='a' && ch<='z'?cout<<"To Lowercase": cout<<"TO Uppercase";
+// }
+
+// int main(){
+//     int table=1;
+
+//     while(table<=10){
+//         cout<<table*2<<endl;
+//         table++;
+//     }
+
+// }
+
+// int main(){
+//     int num=3;
+//     int sum=0;
+
+//     for(int i=0; i<=num; i++){
+//         if (i%2!=0){
+//             sum+=i;  
+//         }
+//     }
+//      cout<<"Sum of odd number:- "<<sum;
+// }
